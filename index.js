64e58b50e99d7e277f1fb1add89ab0a9f030da15
tehila-raviv <tehilaraviv@gmail.com>
2026-04-17 13:06:26 +0300

    refactor: separate express app from server for testing support

--- index.js
+++ index.js
@@ -22,6 +22,10 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Streamlytics running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'test') {
+  app.listen(PORT, () => {
+    console.log(`Streamlytics running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
